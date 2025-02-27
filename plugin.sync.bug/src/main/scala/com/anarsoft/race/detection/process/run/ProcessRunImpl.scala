fa445fd8fc29684c88f1f6d3642fb72ac327b5b4
Thomas Krieger <thomas.krieger@vmlens.com>
2025-02-27 10:42:02 +0100

    stacktrace for non volatile events

--- plugin.sync.bug/src/main/scala/com/anarsoft/race/detection/process/run/ProcessRunImpl.scala
+++ plugin.sync.bug/src/main/scala/com/anarsoft/race/detection/process/run/ProcessRunImpl.scala
@@ -11,37 +11,30 @@ import scala.collection.mutable.ArrayBuffer
 class ProcessRunImpl extends ProcessRun {
 
   def process(runData: RunData): RunResult = {
-    /*
-    check run terminated (e.g. junit assertion failed)
-    should createreport
-    createreport sync action
-    createreport memory
-    should createreport further
-    createreport methods
-    set stacktraces
-    addMethodEvents to runBuilder
-    addMethodEvents runBuilder to loop builder
-     */
-
-
+    // calculate the stacktrace map
     val stackTraceIdToStacktrace = new ServiceCalculateMethodCountToStacktraceNode().process(runData.methodEventArray);
+    val stackTraceMap = stackTraceIdToStacktrace.toMap;
+
+    // set the stacktrace nodes in the events
     for (syncActionElement <- runData.syncActionElements) {
-      syncActionElement.setStacktraceNode(stackTraceIdToStacktrace.toMap)
+      syncActionElement.setStacktraceNode(stackTraceMap)
+    }
+    for (nonVolatileElement <- runData.nonVolatileElements) {
+      nonVolatileElement.setStacktraceNode(stackTraceMap)
     }
 
-
+    // calculate the partial order
     val partialOrderContainer = new PartialOrderContainer();
-
     for (syncActionElement <- runData.syncActionElements) {
       syncActionElement.addToPartialOrderBuilder(partialOrderContainer);
     }
 
+    // sort the non volatile events according to the partial order
     val partialOrder = new PartialOrderImpl(partialOrderContainer);
     val nonVolatileResult = new ArrayBuffer[GroupNonVolatileMemoryAccessElementForResult]();
     for (nonVolatileElement <- runData.nonVolatileElements) {
       nonVolatileResult.append(nonVolatileElement.sort(partialOrder));
     }
-
 
     new RunResultImpl(runData.loopAndRunId, nonVolatileResult.toList, runData.controlEvents,
       runData.syncActionElements);
